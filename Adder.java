cd602cf28b640fe363b0075254789bc07d18af7c
Anshul Gupta <guptaanshul435@gmail.com>
2024-07-27 18:44:11 +0530

    second commit

--- Adder.java
+++ Adder.java
@@ -2,10 +2,15 @@ class Adder{
 public staic void main(String[] args){
 
 System.out.println("The addtion of the tow numbers is "+adder(10,20));
+System.out.println("The addtion of the tow numbers is "+adder(20,10));
 
 }
 public int adder(int first, int second){
 return first+second;
 
+}
+
+public int subtaction(int first,int second){
+	return first-second;
 }
 }
