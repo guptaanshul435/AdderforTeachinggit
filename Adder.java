97ce64c3960d98e175b91e4b17ba2b2d1e357ddf
Anshul Gupta <guptaanshul435@gmail.com>
2024-07-27 18:13:41 +0530

    testing commit

---
+++ Adder.java
@@ -0,0 +1,11 @@
+class Adder{
+public staic void main(String[] args){
+
+System.out.println("The addtion of the tow numbers is "+adder(10,20));
+
+}
+public int adder(int first, int second){
+return first+second;
+
+}
+}
